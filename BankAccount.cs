ff4d438da3348f6d975b3c5ae669d11ab6e78300
Jeremy <jeremy.frank@students.snow.edu>
2026-09-23 09:45:34 -0600

    Complete bank account encapsulation lab

---
+++ BankAccount.cs
@@ -0,0 +1,69 @@
+﻿public class BankAccount
+{
+    // Private fields protect account data from being changed directly
+    private string accountNumber;
+    private decimal balance;
+
+
+    public string AccountNumber
+    {
+        get
+        {
+            return accountNumber;
+        }
+    } 
+
+    public decimal Balance
+    {
+        get
+        {
+            return balance;
+        }
+    }
+
+    public BankAccount(string accountNumber, decimal initialBalance)
+    {
+        this.accountNumber = accountNumber;
+
+        // Prevent an account from starting with a negative balance
+        if (initialBalance < 0)
+        {
+            balance = 0;
+        }
+        else
+        {
+            balance = initialBalance;
+        }   
+
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        }
+
+        else
+        {
+            Console.WriteLine(" Deposit amount is invalid");
+        }
+      
+    }
+
+    public void Withdraw(decimal amount)
+    {
+
+        // Only withdraw when the amount is valid and enough money is available
+        if ( amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+
+        }
+        else
+        {
+            Console.WriteLine("Error: Insufficient Funds");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿BankAccount account = new BankAccount("123456" ,500 );
+
+Console.WriteLine($"Account Number: {account.AccountNumber:F2}");
+Console.WriteLine($"Balance: ${account.Balance:F2}\n");
+
+Console.WriteLine("Depositing $100...");
+account.Deposit(100);
+Console.WriteLine($"Balance: ${account.Balance:F2}\n");
+
+Console.WriteLine("Withdrawing $200...");
+account.Withdraw(200);
+Console.WriteLine($"Balance ${account.Balance:F2}\n");
+
+Console.WriteLine("Withdrawing $1000...");
+account.Withdraw(1000);
+Console.WriteLine($"Balance ${account.Balance:F2}");
